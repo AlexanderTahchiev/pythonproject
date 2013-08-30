b661e2d50bef1838b3bed9f8e898e986d458cf37
AlexanderTahchiev <alexander.tahchiev@gmail.com>
2013-08-30 08:07:26 +0300

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,10 @@ def menu():
 
 pygame.init()
 
+
 class Colors:
+
+    """klas za cvetovete"""
     YELLOW = (255, 255, 0)
     BLACK = (0, 0, 0)
     GREEN = (0, 255, 0)
@@ -22,8 +25,8 @@ WINDOWHEIGHT = 1200
 WINDOWLENGTH = 800
 POINTRADIUS = 3
 
-NUMBER_OF_POINTS = 15
-DISTANCE_FROM_POINT = 10
+NUMBER_OF_POINTS = 5
+DISTANCE_FROM_POINT = 5
 
 mousex = 0
 mousey = 0
@@ -33,8 +36,19 @@ DISPLAYSURF.fill(Colors.BLACK)
 pygame.display.set_caption('THE GAME!')
 
 
+class AI:
+
+    def __init__(self, name, turn, points, pos):
+        self.turn = turn
+        self.points = points
+        self.name = name
+        self.pos = pos
+
 
 class Player:
+
+    """Vsichko nujna informacia koqto trqbva da se sledi za igracha
+    na tozi etap """
 
     def __init__(self, name, turn, points, pos):
         self.turn = turn
@@ -42,8 +56,7 @@ class Player:
         self.name = name
         self.pos = pos
     old_points = 0
-    def change_turn(self):
-        return not turn
+    number_of_drawen_points = 0
 
     def get_points(self):
         return self.points
@@ -57,7 +70,6 @@ class Player:
             if i[0] == last_line_ends[0] or i[1] == last_line_ends[0]:
                 all_line_from_point.append(i)
         return all_line_from_point
-
 
     def in_event_of_point(self, other, last_line_ends):
         point1 = last_line_ends[0]
@@ -75,17 +87,27 @@ class Player:
         else:
             if number_of_made_points > 2:
                 self.points = self.points + 2
-            else:    
+            else:
                 self.points = self.points + number_of_made_points
+
+    def display_turn(self):
+        if self.moi_red_li_e():
+            clear_turn = pygame.draw.polygon(DISPLAYSURF, Colors.RED, [
+                                             (450 - 20, 10 - 5), (450 + 120, 10 - 5), (450 + 120, 10 + 20), (450 - 20, 10 + 20)])
+            number_of_drawen_points = len(all_points)
+            whos_turn = myfont.render(self.name + " turn", 1, Colors.YELLOW)
+            return DISPLAYSURF.blit(whos_turn, (450, 10))
 
     def display_points(self):
         poin = myfont.render(str(self.points), 1, Colors.YELLOW)
         if self.old_points < self.points:
-        
-            pygame.draw.polygon(DISPLAYSURF, Colors.BLACK, [(self.pos[0]-7, self.pos[1]-7),(self.pos[0]+20, self.pos[1]-7),(self.pos[0]+20, self.pos[1]+20),(self.pos[0]-7, self.pos[1]+20)])
+
+            pygame.draw.polygon(DISPLAYSURF, Colors.BLACK, [(self.pos[0] - 7, self.pos[1] - 7), (
+                self.pos[0] + 20, self.pos[1] - 7), (self.pos[0] + 20, self.pos[1] + 20), (self.pos[0] - 7, self.pos[1] + 20)])
 
             self.old_points = self.points
-        return DISPLAYSURF.blit(poin, self.pos)     
+        return DISPLAYSURF.blit(poin, self.pos)
+
     def get_stats(self):
         print(self.name, ":", self.points, " ", self.turn)
 
@@ -98,8 +120,6 @@ DISPLAYSURF.blit(Pl2, (135, 10))
 
 Player1 = Player("Player1", True, 0, (100, 10))
 Player2 = Player("Player2", False, 0, (230, 10))
-
-
 
 
 def main():
@@ -117,10 +137,13 @@ def main():
 
     CAN_WE_LINE = False
     while len(all_points_centers) < NUMBER_OF_POINTS:
+        """Purviq cikul dokato se napravqt n na broi tochki"""
         menu()
         for event in pygame.event.get():  # event handling loop
             Player1.display_points()
             Player2.display_points()
+            Player1.display_turn()
+            Player2.display_turn()
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 terminate()
             elif event.type == MOUSEBUTTONUP:
@@ -131,8 +154,12 @@ def main():
                     if compare_all_points_with_new_one(all_points_centers, (mousex, mousey)):
                         pass
                     else:
+                        Player1.turn = not Player1.turn
+                        Player2.turn = not Player2.turn
                         draw_point(mousex, mousey, Colors.GREEN)
                 else:
+                    Player1.turn = not Player1.turn
+                    Player2.turn = not Player2.turn
                     draw_point(mousex, mousey, Colors.GREEN)
 
             pygame.display.update()
@@ -149,17 +176,17 @@ def main():
     print("all_lines_ends", all_lines_ends)
     last_line_ends = []
     while True:  # main game loop
-        
+        """cikul do kogato se stigne kraq na igrata"""
         for event in pygame.event.get():
             Player1.display_points()
             Player2.display_points()
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 terminate()
+
             elif event.type == MOUSEBUTTONUP:
                 mousex, mousey = (event.pos[0], event.pos[1])
                 if IfPointAtClick(mousex, mousey) and CAN_WE_LINE:
                     last_line_ends.append(IfPointAtClickReturn(mousex, mousey))
-
                     if can_we_lineup(last_line_ends):
                         add_forbiden_points(last_line_ends)
                         second_point = IfPointAtClickReturn(mousex, mousey)
@@ -170,10 +197,14 @@ def main():
                             Player1.in_event_of_point(Player2, last_line_ends)
                         else:
                             Player2.in_event_of_point(Player1, last_line_ends)
+                        print(Player1.turn)
+                        print(Player2.turn)
                         Player1.get_stats()
                         Player2.get_stats()
                         draw_line(
                             DISPLAYSURF, Colors.GREEN, first_point, second_point, 5)
+                        Player2.display_turn()
+                        Player1.display_turn()
                         second_point = False
                     elif not can_we_lineup(last_line_ends):
                         draw_point(
@@ -188,24 +219,25 @@ def main():
                     first_point = IfPointAtClickReturn(mousex, mousey)
                     draw_point(first_point[0], first_point[1], Colors.RED)
                     CAN_WE_LINE = True
+
             if not did_it_end():
                 if Player1.get_points() > Player2.get_points():
-                    winner  = myfont.render("Player1 won", 1, Colors.YELLOW)
-                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 100 ,  10))
+                    winner = myfont.render("Player1 won", 1, Colors.YELLOW)
+                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 100,  10))
                     print("Player1 won")
                 elif Player1.get_points() < Player2.get_points():
                     winner = myfont.render("Player2 won", 1, Colors.YELLOW)
-                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 150 ,  10))
+                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 150,  10))
                     print("Player2 won")
                 else:
                     winner = myfont.render("Draw", 1, Colors.YELLOW)
-                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 150 ,  10))
+                    DISPLAYSURF.blit(winner, (WINDOWHEIGHT - 150,  10))
                     print("Draw")
         pygame.display.update()
 
 
-
 def IfPointAtClick(mousex, mousey):
+    """ako e cuknata tochka vrushta True"""
     for i in prctective_area_of_points:
         if i.collidepoint(mousex, mousey):
             for j in all_points_centers:
@@ -215,6 +247,7 @@ def IfPointAtClick(mousex, mousey):
 
 
 def IfPointAtClickReturn(mousex, mousey):
+    """ako e cuknata tochka vrushta cukanata tochka"""
     for i in prctective_area_of_points:
         if i.collidepoint(mousex, mousey):
             for j in all_points_centers:
@@ -223,6 +256,7 @@ def IfPointAtClickReturn(mousex, mousey):
 
 
 def draw_point(mousex, mousey, color):
+    """risuva tochka"""
     if (mousex, mousey) in all_points_centers:
         pygame.draw.circle(
             DISPLAYSURF, color, (mousex, mousey), POINTRADIUS, 0)
@@ -261,14 +295,11 @@ def making_dict_of_points_names():
         map_between_point_forbpoints[i] = [i]
     return map_between_point_forbpoints
 
-
-def returned_points(mousex, mousey, dict_of_points_centers):
-    return dict_of_canters_points[(mousex, mousey)]
-
 # zapisva q po dvata nachina (A,B), (B,A)
 
 
 def draw_line(surf, color, point1, point2, thickness, ):
+    """risuva prava i dobavq nqkoi neini harekteristiki v struturi ot danni"""
     if (point1, point2) in all_lines_ends:
         pass
     elif point1 == point2:
@@ -278,12 +309,13 @@ def draw_line(surf, color, point1, point2, thickness, ):
         all_lines_ends.append((point2, point1))
         all_lines.append(pygame.draw.line(
             DISPLAYSURF, Colors.GREEN, point1, point2, thickness))
-        return pygame.draw.line(DISPLAYSURF, Colors.GREEN, point1, point2, thickness)
+        return pygame.draw.line(DISPLAYSURF, color, point1, point2, thickness)
 
 
 # nad tezi dam islq
 
 def caluclate_line_by_2_points(point1, point2):
+    """namira naklona i konstanta kum dadena prava"""
     if point1[0] == point2[0]:
         return 0, point1[1]
     a = (point1[1] - point2[1]) / (point1[0] - point2[0])
@@ -292,17 +324,18 @@ def caluclate_line_by_2_points(point1, point2):
 
 
 def add_forbiden_points(last_line_ends):
+    """sprqmo poslednata prava 
+    preglejda vsichki tochki 
+    i dobavq v rechnika na zabranenite tochki sprqmo nqkoq tochka"""
     point1 = last_line_ends[0]
     point2 = last_line_ends[1]
     pot_line = caluclate_line_by_2_points(point1, point2)
     if point1 == point2:
         pass
     else:
-
         for i in all_points_centers:
             for j in all_points_centers:
                 rand_line = caluclate_line_by_2_points(i, j)
-                # ako sa v edna polu ravnina i po golemi ot pravata i
                 if i == j or i in last_line_ends or j in last_line_ends:
                     pass
                 elif i in last_line_ends and j not in last_line_ends:
@@ -331,11 +364,8 @@ def add_forbiden_points(last_line_ends):
                         set(map_between_point_forbpoints[point_key1]))
 
 
-def calculate_line_value(a_b, point):
-    return round(a_b[0] * point[0] + a_b[1])
-
-
 def ispnt_underorabove(line, point):
+    """v koq poluravnina e dadena tochka sprqmo dadena prava"""
     if line[0] * point[0] + line[1] < point[1]:
         return 1
     elif line[0] * point[0] + line[1] > point[1]:
@@ -345,6 +375,7 @@ def ispnt_underorabove(line, point):
 
 
 def can_we_lineup(last_line_ends):
+    """dali novata prava ne presicha veche sushtestvuvashta"""
     point1 = last_line_ends[0]
     point2 = last_line_ends[1]
     p1 = dict_of_canters_points[point1]
@@ -364,6 +395,7 @@ def can_we_lineup(last_line_ends):
 
 
 def IfPointAreaAtClick(mousex, mousey):
+    """dali krukcheto okolo tochkata e clicknato"""
     for i in prctective_area_of_points:
         if i.collidepoint(mousex, mousey):
             return True
@@ -371,11 +403,14 @@ def IfPointAreaAtClick(mousex, mousey):
 
 
 def dist_between_point_and_line(point1, point2, point3):
+    """presmqta raztoqnieto ot tochka do prava"""
     lineeq = caluclate_line_by_2_points(point1, point2)
     return round(abs((lineeq[0] * point3[0] - point3[1] + lineeq[1]) / (((lineeq[0]) ** 2 + 1) ** 0.5)))
 
 
 def compare_all_points_with_new_one(all_points_centers, new_point_center):
+    """ proverqva dali novo napravenata 
+    tochka leji na edna prava sus sushtestvuvashtite tochki"""
     n = len(all_points_centers)
     for i in range(n):
         for j in range(i + 1, n):
@@ -383,18 +418,21 @@ def compare_all_points_with_new_one(all_points_centers, new_point_center):
                 return True
     return False
 
+
 def did_it_end():
+    """dali e krai na igrata"""
     for i in all_points_centers:
         for j in all_points_centers:
-            the_line = (i,j)
+            the_line = (i, j)
             if i == j:
                 pass
             elif can_we_lineup(the_line):
                 return True
     return False
 
-# tezi hich ne gi polzvam ma da sedqt
+
 def IfLineAtClick(mousex, mousey):
+    """ako shtqh da razhirqvam tva shteshe da iam smisul"""
     for i in prctective_area_of_lines:
 
         if i.collidepoint(mousex, mousey):
@@ -403,6 +441,7 @@ def IfLineAtClick(mousex, mousey):
 
 
 def PointVector(point):
+    """ako shtqh da razhirqvam tva shteshe da iam smisul"""
     x = point[0]
     y = point[1]
     r = (x ** 2 + y ** 2) ** 0.5
